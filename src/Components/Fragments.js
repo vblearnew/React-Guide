331f2c633c453aeb34222236af236a6618a5932a
Vignesh Balaji <vblearnew@gmail.com>
2020-08-29 23:00:43 +0530

    Fragments changes

--- src/Components/Fragments.js
+++ src/Components/Fragments.js
@@ -3,9 +3,10 @@ import React from 'react'
 class Fragments extends React.Component{
  render(){
      return(
-         <div className="Content">
+         <React.Fragment>
             <h1 class="center-align blue-grey-text">Fragments</h1>
-         </div>
+            A common pattern in React is for a component to return multiple elements. Fragments let you group a list of children without adding extra nodes to the DOM.
+         </React.Fragment>
      )
  }
 }
